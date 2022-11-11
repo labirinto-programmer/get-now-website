0ce3c4333abdd287ba032c22d26c5717ef8b6863
navi-prog <naviprog139@gmail.com>
2022-11-11 13:50:36 +0300

    Update folder of client

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <h1>hello there</h1>
+    </BrowserRouter>
+  );
+}
+
+export default App;

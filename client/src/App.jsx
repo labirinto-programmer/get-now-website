0ee60e7ac39fef725fd21028b4fd29dfd1a3f5c4
navi-prog <naviprog139@gmail.com>
2022-11-21 17:57:27 +0300

    Create Router folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
+import Routers from './routes/Routers';
 import './App.css';
-import {Home} from './pages/index';
 
 function App() {
   return (
     <BrowserRouter>
-      <Home/>
+      <Routers/>
     </BrowserRouter>
   );
 }

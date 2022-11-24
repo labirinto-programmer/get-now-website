17fe86dfa8e5dfec488836a83ead611720e87b65
labirinttoo <mylabirinto465@gmail.com>
2022-11-24 06:05:00 +0300

    Change on some files & add data folder

--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -1,5 +1,5 @@
-export {default as Container} from './Container/Container';
-export {default as MainTitle} from './MainTitle/MainTitle';
-export {default as Card} from './Card/Card';
-export {default as ButtonCard} from './Button/Button';
-export {ButtonCardSelect} from './Button/Button';
+export {default as Container} from './container/Container';
+export {default as MainTitle} from './mainTitle/MainTitle';
+export {default as Card} from './card/Card';
+export {default as ButtonCard} from './button/Button';
+export {ButtonCardSelect} from './button/Button';
